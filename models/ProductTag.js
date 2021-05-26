2cc3d1f903c2878839eec1895e6787c264cb6c2c
Mohit Arora <mohit.arora@sydney.edu.au>
2021-05-27 00:34:33 +1000

    productTag models completed

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,35 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// Initialize Product model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
+// set up fields and rules for Product model
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer value
+      allowNull: false, // doesn't allow null values
+      primaryKey: true, // set as a primary key
+      autoIncrement: true // uses auto increment
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // integer value
+      references: {
+        model: 'product', // This references the `product` model, which we set in `Product.js` as its `modelName` property
+        key: 'id' //takes product id
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // integer value
+      references: {
+        model: 'tag', // This references the `tag` model, which we set in `Tag.js` as its `modelName` property
+        key: 'id' //takes tag id
+      }
+    } 
   },
   {
     sequelize,
