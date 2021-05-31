731dd8f5934d21ff98ef17687479bf79227c3513
Mohit Arora <mohit.arora@sydney.edu.au>
2021-05-31 21:22:48 +1000

    added category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,17 +6,17 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   try { 
-    const CatData = await Category.findAll({
+    const catData = await Category.findAll({
     include: {  // include its associated Products
       model: Product,
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
-  if(!CatData[0]) {
-        res.status(404).json({message: 'This categories does not exist'}); //status code for wrong query
+  if(!catData[0]) {
+        res.status(404).json({message: 'This categories do not exist'}); //status code for wrong query
         return;
       }
-      res.status(200).json(CatData); // status code for the response
+      res.status(200).json(catData); // status code for the response
     }
     catch (err) {
       res.status(500).json(err) //status code for Internal Server Error, it is a generic "catch-all" response
@@ -25,19 +25,73 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try { 
+    const catData = await Category.findOne({
+    Where: { 
+      id: req.params.id 
+    },
+    include: {  // include its associated Products
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  if(!catData[0]) {
+        res.status(404).json({message: 'This category does not exist'}); //status code for wrong query
+        return;
+      }
+      res.status(200).json(catData); // status code for the response
+    }
+    catch (err) {
+      res.status(500).json(err) //status code for Internal Server Error, it is a generic "catch-all" response
+    }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const catData = await User.create({
+      category_name: req.body.category_name
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData[0]) {
+      res.status(404).json({ message: 'No category exists with this id!' });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData) {
+      res.status(404).json({ message: 'No category exists with this id!' });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
