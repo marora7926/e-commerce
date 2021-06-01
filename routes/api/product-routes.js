7b48cb8a96fbe1964bc968e7a3d12e38c781f37e
Mohit Arora <mohit.arora@sydney.edu.au>
2021-06-01 15:02:45 +1000

    updated product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -62,12 +62,7 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  Product.create({
-    product_name: req.body.product_name, // req.body should look like this...
-    price: req.body.price,
-    stock: req.body.stock,
-    tagIds: req.body.tagIds
-  })
+  Product.create(req.body)
   .then((product) => {
     // if there's product tags, we need to create pairings to bulk create in the ProductTag model
     if (req.body.tagIds.length) {
